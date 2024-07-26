80fc17c4aff4c279a4b05b6af219d557bef25e40
MumuKim0212 <167409133+MumuKim0212@users.noreply.github.com>
2024-07-26 12:08:48 +0900

    1

--- Algorithm/Maze/Maze.cpp
+++ Algorithm/Maze/Maze.cpp
@@ -1,7 +1,21 @@
 #include "pch.h"
+#include <iostream>
+#include "ConsoleHelper.h"
+#include "Board.h"
+#include "Player.h"
+
+Board board;
+Player player;
 
 int main()
 {
+
+	// 맵 설정을 위한 시드 지정
+	::srand(static_cast<unsigned>(time(nullptr)));
+	// 25칸 보드로 초기화
+	board.Init(25, &player);
+	player.Init(&board);
+
 	uint64 lastTick = 0;
 	// 대부분의 게임은 main안에서 반복문이 무한히 반복
 	while (true)
@@ -15,21 +29,9 @@ int main()
 		// 입력 : 키보드, 마우스 입력
 
 		// 로직 : 플레이어의 이동 등 게임로직
+		player.Update(deltaTick);
 
 		// 렌더링 : 게임을 화면에 그려줌
-
-		ConsoleHelper::SetCursorPosition(0, 0);
-		ConsoleHelper::ShowConsoleCursor(false);
-		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
-
-		const char* TILE = "■";
-		for (int32 y = 0; y < 25; y++)
-		{
-			for (int32 x = 0; x < 25; x++)
-			{
-				cout << TILE;
-			}
-			cout << endl;
-		}
+		board.Render();
 	}
 }
--- Algorithm/Maze/pch.h
+++ Algorithm/Maze/pch.h
@@ -1,9 +1,48 @@
 #pragma once
 // Pre Compiled Header. 공용으로 사용할 부분들을 지정
 #include "Types.h"
-#include "ConsoleHelper.h"
 #include <windows.h>
 #include <iostream>
 #include <vector>
 using namespace std;
 
+struct Pos
+{
+	bool operator== (Pos& other)
+	{
+		return y == other.y && x == other.x;
+	}
+
+	bool operator!= (Pos& other)
+	{
+		return !(*this == other);
+	}
+
+	Pos operator+(Pos& other)
+	{
+		Pos ret;
+		ret.y = y + other.y;
+		ret.x = x + other.x;
+		return ret;
+	}
+
+	Pos operator+=(Pos& other)
+	{
+		y += other.y;
+		x += other.x;
+		return *this;
+	}
+	int32 y = 0;
+	int32 x = 0;
+};
+
+// 현재 방향. 반시계방향으로
+enum Dir
+{
+	DIR_UP = 0,
+	DIR_LEFT = 1,
+	DIR_DOWN = 2,
+	DIR_RIGHT = 3,
+
+	DIR_COUNT = 4
+};
