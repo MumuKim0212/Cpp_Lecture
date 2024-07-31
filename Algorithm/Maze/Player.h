ad54c34a868acf1b6edd4a3a3e04a595f23a3315
MumuKim0212 <167409133+MumuKim0212@users.noreply.github.com>
2024-07-31 15:50:35 +0900

    Dijikstra

--- Algorithm/Maze/Player.h
+++ Algorithm/Maze/Player.h
@@ -9,13 +9,17 @@ class Player
 		MOVE_TICK = 100
 	};
 public:
-	void Init(Board* board);
-	void Update(uint64 deltaTick);
+	void			Init(Board* board);
+	void			Update(uint64 deltaTick);
 
-	void SetPos(Pos pos) { _pos = pos; }
-	Pos GetPos() { return _pos; }
+	void			SetPos(Pos pos) { _pos = pos; }
+	Pos				GetPos() { return _pos; }
 
-	bool CanGo(Pos pos);
+	bool			CanGo(Pos pos);
+
+private:
+	void			RightHand();
+	void			Bfs();
 
 private:
 	Pos				_pos = {};
